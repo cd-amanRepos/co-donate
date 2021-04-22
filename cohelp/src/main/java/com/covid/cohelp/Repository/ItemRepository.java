c2f7ee99ec63e4f64cc08db8c40d7cf0d3a72f2b
Aman Thapliyal <amanthapliyal@pop-os.localdomain>
2021-04-22 15:41:44 +0530

    Created Item Repository

--- cohelp/src/main/java/com/covid/cohelp/Repository/ItemRepository.java
+++ cohelp/src/main/java/com/covid/cohelp/Repository/ItemRepository.java
@@ -1,4 +1,9 @@
 package com.covid.cohelp.Repository;
 
-public interface ItemRepository {
+import com.covid.cohelp.Entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }

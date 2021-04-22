ccac0e7fae4954f8d533ca735b97c77dec92fd05
Aman Thapliyal <amanthapliyal@pop-os.localdomain>
2021-04-22 15:42:18 +0530

    Create Post Entity

--- cohelp/src/main/java/com/covid/cohelp/Entity/Post.java
+++ cohelp/src/main/java/com/covid/cohelp/Entity/Post.java
@@ -1,4 +1,34 @@
 package com.covid.cohelp.Entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Post {
+    @Id
+    @GeneratedValue
+    private long postId;
+    private String description;
+    private String owner_name;
+    private long owner_mob;
+    private String owner_email;
+    private String location;
+
+    @ManyToOne
+    @JsonBackReference
+    private Item item;
+
 }

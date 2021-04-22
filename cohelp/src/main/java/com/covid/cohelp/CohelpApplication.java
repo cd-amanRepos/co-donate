5ceca5565bffb5c75459db672c5b6d1e48a27337
Aman Thapliyal <amanthapliyal@pop-os.localdomain>
2021-04-22 14:27:16 +0530

    Add Project

---
+++ cohelp/src/main/java/com/covid/cohelp/CohelpApplication.java
@@ -0,0 +1,13 @@
+package com.covid.cohelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CohelpApplication.class, args);
+	}
+
+}
---
+++ cohelp/src/test/java/com/covid/cohelp/CohelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.cohelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

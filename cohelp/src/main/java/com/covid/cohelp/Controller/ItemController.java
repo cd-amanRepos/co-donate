dceefe05022e519d9328595798481074b5af08ee
Aman Thapliyal <amanthapliyal@pop-os.localdomain>
2021-04-22 15:40:42 +0530

    Item Controller

--- cohelp/src/main/java/com/covid/cohelp/Controller/ItemController.java
+++ cohelp/src/main/java/com/covid/cohelp/Controller/ItemController.java
@@ -1,2 +1,30 @@
-package com.covid.cohelp.Controller;public class ItemController {
+package com.covid.cohelp.Controller;
+
+import com.covid.cohelp.Entity.Item;
+import com.covid.cohelp.Service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/items")
+    public List<Item> getAllItems() {
+        return itemService.getAllItems();
+    }
+    @PostMapping("/item")
+    public Item addItem(@RequestBody Item item) {
+        return itemService.addItem(item);
+    }
+
+    @PutMapping("/item/")
+    public Item updateItem(@RequestBody Item item) {
+        return itemService.updateItem(item);
+    }
+
 }

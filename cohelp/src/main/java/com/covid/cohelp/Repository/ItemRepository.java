e83fcf71b3d012dacee0f6ba1898df526067d0ad
Aman Thapliyal <amanthapliyal@pop-os.localdomain>
2021-04-22 17:09:07 +0530

    New search by ItemName query

--- cohelp/src/main/java/com/covid/cohelp/Repository/ItemRepository.java
+++ cohelp/src/main/java/com/covid/cohelp/Repository/ItemRepository.java
@@ -4,6 +4,9 @@ import com.covid.cohelp.Entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    public Optional<Item> findByName(String item_name);
 }

790f142eb83d50f6c3a940811643c8ae30f4211c
Aman Thapliyal <amanthapliyal@pop-os.localdomain>
2021-04-22 17:06:39 +0530

    Add new field item_name

--- cohelp/src/main/java/com/covid/cohelp/Entity/Post.java
+++ cohelp/src/main/java/com/covid/cohelp/Entity/Post.java
@@ -7,10 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
@@ -29,6 +26,9 @@ public class Post {
 
     @ManyToOne
     @JsonBackReference
+    @JoinColumn(name = "item_id")
     private Item item;
+
+    private String item_name;
 
 }
